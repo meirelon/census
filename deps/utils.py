132ae5c7514e7706fd42353567f139b3da7e1600
meirelon <nestelm@gmail.com>
2020-03-20 22:09:43 -0400

    changed utils

--- deps/utils.py
+++ deps/utils.py
@@ -7,69 +7,98 @@ import numpy as np
 
 import gcsfs
 
-def get_states():
-    url = "https://api.census.gov/data/2010/dec/responserate?get=GEO_ID,FSRR2010&for=state:*"
+def get_states(key, year=2020):
+    url = f"https://api.census.gov/data/2020/dec/responserate?get=GEO_ID,CRRALL,CRRINT,DAVG,DINTAVG,DRRALL,DRRINT&key={key}&for=state:*"
     JSONContent = requests.get(url).json()
-    states = pd.DataFrame(JSONContent)
-    states.columns = states.iloc[0]
-    states = states.iloc[1:,2]
-    states = [int(i) for i in states]
+    temp = pd.DataFrame(JSONContent)
+    temp.columns = temp.iloc[0]
+    temp = temp.iloc[1:,-1]
+    states = [int(i) for i in temp]
 
-    return states
+    labels = pd.DataFrame(JSONContent).iloc[0,:-1]
 
-def get_responses(states):
-    columns=['GEO_ID','FSRR2010']
+    return [states, labels]
+
+def get_responses(states, key, labels, year=2020):
+    # columns=['GEO_ID','FSRR2010']
+    columns=labels
 
     tract_responses = pd.DataFrame(columns=columns)
 
     for i in states:
         if i < 10:
-            url = "https://api.census.gov/data/2010/dec/responserate?get=GEO_ID,FSRR2010&for=tract:*&in=state:0" + str(i)
+            url = f"https://api.census.gov/data/2020/dec/responserate?get=GEO_ID,CRRALL,CRRINT,DAVG,DINTAVG,DRRALL,DRRINT&key={key}&for=tract:*&in=state:0"\
+            + str(i)
         else:
-            url = "https://api.census.gov/data/2010/dec/responserate?get=GEO_ID,FSRR2010&for=tract:*&in=state:" + str(i)
+            url = f"https://api.census.gov/data/2020/dec/responserate?get=GEO_ID,CRRALL,CRRINT,DAVG,DINTAVG,DRRALL,DRRINT&key={key}&for=tract:*&in=state:"\
+            + str(i)
         JSONContent = requests.get(url).json()
         temp = pd.DataFrame(JSONContent)
-        temp = temp.iloc[1:,[0,1]]
-        temp.columns = columns
-        tract_responses = pd.concat([tract_responses,temp])
-        return tract_responses
+        temp.columns = temp.iloc[0]
+        temp = temp.iloc[1:,0:-3]
+        tract_responses = pd.concat([tract_responses,temp],sort=True)
+    return tract_responses
 
-def get_county_responses(states):
-    county_responses = pd.DataFrame(columns=columns)
+def get_county_responses(states, key, labels, year=2020):
+    county_responses = pd.DataFrame(columns=labels)
 
     for i in states:
         if i < 10:
-            url = "https://api.census.gov/data/2010/dec/responserate?get=GEO_ID,FSRR2010&for=county:*&in=state:0" + str(i)
+            url = f"https://api.census.gov/data/2020/dec/responserate?get=GEO_ID,CRRALL,CRRINT,DAVG,DINTAVG,DRRALL,DRRINT&key={key}&key={key}&for=county:*&in=state:0"\
+            + str(i)
         else:
-            url = "https://api.census.gov/data/2010/dec/responserate?get=GEO_ID,FSRR2010&for=county:*&in=state:" + str(i)
+            url = f"https://api.census.gov/data/2020/dec/responserate?get=GEO_ID,CRRALL,CRRINT,DAVG,DINTAVG,DRRALL,DRRINT&key={key}&for=county:*&in=state:"\
+            + str(i)
         JSONContent = requests.get(url).json()
         temp = pd.DataFrame(JSONContent)
-        temp = temp.iloc[1:,[0,1]]
-        temp.columns = columns
-        county_responses = pd.concat([county_responses,temp])
+        temp.columns = temp.iloc[0]
+        temp = temp.iloc[1:,0:-2]
+        county_responses = pd.concat([county_responses,temp],sort=True)
 
         return county_responses
 
-def get_state_responses():
-    url = "https://api.census.gov/data/2010/dec/responserate?get=GEO_ID,FSRR2010&for=state:*"
+def get_state_responses(key, year=2020):
+    url = f"https://api.census.gov/data/2020/dec/responserate?get=GEO_ID,CRRALL,CRRINT,DAVG,DINTAVG,DRRALL,DRRINT&key={key}&for=state:*"
     JSONContent = requests.get(url).json()
     state_responses = pd.DataFrame(JSONContent)
     state_responses.columns = state_responses.iloc[0]
-    state_responses = state_responses.iloc[1:,0:2]
+    state_responses = state_responses.iloc[1:,0:-1]
 
     return state_responses
 
 
-def run(bucket_name):
+def run():
+    year=2020
+    key = '2988f01f5e86175bda8beae2b5035e1ccef2d052'
     dt = datetime.now().strftime("%Y%m%d")
-    states = get_states()
-    tract_responses = get_responses(states)
+
+    states, labels = get_states(key, year)
+    tract_responses = get_responses(states, key, labels, year)
+    tract_responses.to_csv(f"gs://twitter_bots/tract_responses_{dt}.csv", index=False)
+
+    state_responses = get_state_responses(key, year)
+    state_responses.to_csv(f"gs://twitter_bots/state_responses_{dt}.csv", index=False)
+
+    county_responses = get_county_responses(states, key, labels, year)
+    county_responses.to_csv(f"gs://twitter_bots/county_responses_{dt}.csv", index=False)
+
+
+    return "True"
+
+
+def run(bucket_name):
+    year=2020
+    key = '2988f01f5e86175bda8beae2b5035e1ccef2d052'
+    dt = datetime.now().strftime("%Y%m%d")
+
+    states, labels = get_states(key, year)
+    tract_responses = get_responses(states, key, labels, year)
     tract_responses.to_csv(f"gs://{bucket_name}/tract_responses_{dt}.csv", index=False)
 
-    state_responses = get_state_responses()
+    state_responses = get_state_responses(key, year)
     state_responses.to_csv(f"gs://{bucekt_name}/state_responses_{dt}.csv", index=False)
 
-    county_responses = get_county_responses(states)
+    county_responses = get_county_responses(states, key, labels, year)
     county_responses.to_csv(f"gs://{bucket_name}/county_responses_{dt}.csv", index=False)
 
 
